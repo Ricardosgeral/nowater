5bcde138421f7f35a3148f11dd9146076f8792ff
Ricardosgeral <ricardos.geral@gmail.com>
2019-01-22 18:36:40 +0000

    improvment

--- main.py
+++ main.py
@@ -1,21 +1,49 @@
 import time
 from machine import Pin  #control GPIOs of micro controler
+import RGBled
 
 def main():
-    led_buz = Pin(2, Pin.OUT)
-    water_detector = Pin(27,Pin.IN,Pin.PULL_UP)
-    led_buz.value(0)
+    buz = Pin(4, Pin.OUT)                           # buzzer
+    water_detector = Pin(27,Pin.IN,Pin.PULL_UP)     # output from non-contact water sensor
+    pullup = Pin(12,Pin.OUT)                        # to detect if the non-contact water sensor is working
+
+    #turn off all leds and buzzer
+    buz.value(0)
+    RGBled.redOff()
+    RGBled.greenOff()
+    RGBled.blueOff()
+
+    # initial run
+    for i in range(5):
+        RGBled.magentaOn()
+        time.sleep(0.3)
+        RGBled.magentaOff()
+        time.sleep(0.3)
 
     while True:
-        if water_detector.value()==1:
+        if  pullup.value()==1:                  #the water sensor is not connected or is broken!  Yellow alert
+            while pullup.value()==1:
+                RGBled.greenOff()
+                RGBled.yellowOn()
+                time.sleep(0.5)
+                RGBled.yellowOff()
+                time.sleep(0.5)
+                buz.value(0)
+
+        if water_detector.value()==1:           # no water  - RED alert
             while water_detector.value()==1:
-                led_buz.value(1)
-                time.sleep(0.3)
-                led_buz.value(0)
-                time.sleep(0.3)
-        else:
-            led_buz.value(0)
-        time.sleep(0.3)
+                RGBled.greenOff()
+                buz.value(1)
+                RGBled.redOn()
+                time.sleep(0.5)
+                buz.value(0)
+                RGBled.redOff()
+                time.sleep(0.5)
+        else:                                   # water is detected   - GREEN light
+            buz.value(0)
+            RGBled.greenOn()
+
+        time.sleep(0.2)                         # small interval
 
 if __name__ == '__main__':
     main()
