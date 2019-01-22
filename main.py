fee350e6b9ec5d17d24472499eefa3e56d469c79
Ricardosgeral <ricardos.geral@gmail.com>
2019-01-22 11:45:42 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import time
+from machine import Pin
+
+def main():
+    led_buz = Pin(2, Pin.OUT)
+    water_detector = Pin(27,Pin.IN,Pin.PULL_UP)
+    led_buz.value(0)
+
+    while True:
+        if water_detector.value()==1:
+            while water_detector.value()==1:
+                led_buz.value(1)
+                time.sleep(0.3)
+                led_buz.value(0)
+                time.sleep(0.3)
+        else:
+            led_buz.value(0)
+        time.sleep(0.3)
+
+if __name__ == '__main__':
+    main()
